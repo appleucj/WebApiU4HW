2df937ec55339da183f5a8b29a45abf2f98c75fa
appleucj <58881608+appleucj@users.noreply.github.com>
2020-01-25 18:03:57 -0500

    redo

---
+++ new.js
@@ -0,0 +1,105 @@
+// starting data
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["alerts", "strings", "booleans", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    }, 
+    // {  title: "aaaaa",
+    //   choices: ["a","b","c","d"],
+    //   answer:"a"
+    // },
+    // {  title: "bbbbb",
+    //   choices: ["a","b","c","d"],
+    //   answer:"b"
+    // },
+    // {  title: "ccccc",
+    //   choices: ["a","b","c","d"],
+    //   answer:"c"
+    // },
+  ]
+
+var questionsCounter = 0;
+var description  = document.getElementById("description");
+var timer;
+var timeLine = 5*(questions.length+1);
+ // create input area
+var submit = document.createElement("BUTTON");
+submit.innerHTML="submit";
+submit.setAttribute("id", "submit");
+// create submit button
+var inputName = document.createElement("input");
+inputName.setAttribute("type", "text");
+inputName.setAttribute("value", "Enter your initial here:");
+
+// onclick function
+$("#start").on("click", function(){
+  $("#start").remove();
+    //set up timer
+  timer=setInterval(function(){
+  timeLine--
+  $("#timer").text("Remaining Time "+timeLine);
+      if (timeLine === 0){
+          $("#title").empty();
+          $("#title").text("All Done!");
+          $("#description").empty();
+          $("#description").text("Your score is: "+score);
+          $("#timer").remove();
+          $("#result").empty();             
+          $("#result").text("Enter Initial: ");
+          $("#result").append(inputName);                      
+          document.body.append(submit);
+      }
+  }
+   ,1000);
+      counter();
+});
+
+//high score page
+      //  $("#submit").addEventListener("click", hspage());
+      // function hspage (){
+      //     console.log("hspage");
+      // }
+
+function counter(){
+var currentQuestion = questions[questionsCounter];
+$("#title").text(questions[questionsCounter].title);
+$("#description").empty();
+
+for (j=0; j<4;j++){
+  var choicesEl=document.getElementById("description");
+  var choice = currentQuestion.choices[j];
+  var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = j + 1 + ". " + choice;
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+};
+};
+var score = 0;
+function questionClick(){
+  if (this.value === questions[questionsCounter].answer){
+   score += 30;
+     $("#result").text("Correct! Your score is:" +score+".      When time run out will jump to next page.") ;
+   }
+   else{   
+      $("#result").text("Wrong, Your score is:" +score+".      When time run out will jump to next page.") ;
+   }
+   questionsCounter++;
+   counter();
+};
+
+
+  
+
+
+
+
--- u4HW.js
+++ u4HW.js
@@ -1,4 +1,4 @@
-// get user input by onclick
+
 
 var gameArea = document.getElementById('gameArea')
 var startGameButton = document.getElementById('start-game')
@@ -6,82 +6,102 @@ var startGameButton = document.getElementById('start-game')
 
 //start data
 var questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: [ "alerts","strings", "booleans", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    
-  ];
-  var index = 0
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["alerts", "strings", "booleans", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },  {
+    title: "aaaaaaaa",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  }
+
+];
+var index = 0
 
 
-  document.querySelector("#show-game").style="display:none";//invisivle
-  document.querySelector("#start-game").style="display:''"//visible
-
+document.querySelector("#show-game").style = "display:none";//invisible
+document.querySelector("#start-game").style = "display:''"//visible
+document.querySelector("#Highscores").style = "display:'none'"
 
 //step1 - check the data-answer in the button with the user click
 // if the data-answer matches the click, display correct and go to the ////next right away  (index++)
-//if wrong click, substract 15 and go to next question (index++)
+//if wrong click, substract 15 from current time and go to next question (index++)
 
+// onclick start game
+document.querySelector("#start-game").addEventListener("click", function () {
+  document.querySelector("#show-game").style = "display:''"
+  document.querySelector("#start-game").style = "display:none"
+ initializeGame()
+})
+//helper functions
+var startGame = function () {
+  //startover with new choices
+  document.querySelector('#choices').innerHTML = ""
+gameArea.innerHTML=""
 
-  document.querySelector("#start-game").addEventListener("click",function(){
-    document.querySelector("#show-game").style="display:''"
-    document.querySelector("#start-game").style="display:none"
-    myFunction()
-  })
-  //helper functions
-  var startGame = function(){
-    //startover with new choices
-    document.querySelector('#choices').innerHTML=""
+  //add the title of the game
+  //create <h2>question title</h2>
+  var questionTitle = document.createElement('h2');
+  //build the content
+  questionTitle.textContent = questions[index].title;
+  //display the data into the game area
+  gameArea.appendChild(questionTitle);
 
-
-    //add the title of the game
-    //create h2
-    var questionTitle = document.createElement('h2');
-    //build the content
-    questionTitle.textContent = questions[index].title;
-    //display the data into the game area
-    gameArea.appendChild(questionTitle); 
+  //add the choice of the game
+  //create h3
+  var questionChoice = document.createElement('h3');
+  //build the content
+  questionChoice.textContent = questions[index].choices;
+  //display the data into the game area
+  gameArea.appendChild(questionChoice);
+/* "alerts", "strings", "booleans", "numbers"
+<div id="choices">
+<button data-answer="alerts">alerts</buttton>  , i=0
+<button data-answer="alerts">strings</buttton>  , i=1
+<button data-answer="alerts">booleans</buttton>  , i=2
+<button data-answer="alerts">numbers</buttton>  , i=3
+</div>
+*/
+for (let i = 0; i <  questions[index].choices.length; i++) {
+  var   choiceBtn = document.createElement("button")
+  choiceBtn.textContent = questions[index].choices[i]
+  choiceBtn.setAttribute("data-answer", questions[index].answer)
+  choiceBtn.setAttribute("class","choices")
+  choiceBtn.onclick=checkAnswer
+  document.querySelector("#choices").appendChild(choiceBtn)
   
-    //add the choice of the game
-    //create h3
-    var questionChoice = document.createElement('h3');
-    //build the content
-    questionChoice.textContent = questions[index].choices;
-    //display the data into the game area
-    gameArea.appendChild(questionChoice); 
-
-
-    var choice1 = document.createElement("button")
-    choice1.textContent=questions[index].choices[0]
-    choice1.setAttribute("data-answer",questions[index].answer)
-     document.querySelector("#choices").appendChild(choice1)
-      
-     var choice2 = document.createElement("button")
-     choice2.textContent=questions[index].choices[1]
-     choice2.setAttribute("data-answer",questions[index].answer)
-    document.querySelector('#choices').appendChild(choice2)
-    
+}
+  
 }
 
-
+function checkAnswer(){
+   var getAnswer=this.getAttribute("data-answer")
+    var choices= document.querySelector(".choices").textContent
+    if(getAnswer===choices){
+      document.getElementById("results").innerHTML="Correct"
+    }
+    else{
+      document.getElementById("results").innerHTML="Wrong"
+    
+    }
+}
 
 // startGame()
 // function display choices
 //  var choiceBtn = function(){
-   
+
 //  }
 
 // //
 
 // //  function check answer
- 
+
 //  choiceButton.addEventListener('click',checkChoice)
 
 
@@ -91,25 +111,27 @@ var questions = [
 // control timer
 
 
-var numberCount=0;
-var remainingTime = questions.length*15
+var numberCount = 0;
+var remainingTime = questions.length * 15
 // timer
-function myFunction(){
-   
-    startGame()
-    setInterval(function(){
-        document.getElementById("timer").innerHTML="<b>"+ remainingTime+"</b>"
-        console.log(remainingTime--)
-        
-      numberCount++
-       if(numberCount===15 &&  index < questions.length ){
-       
-          index++
-          numberCount=0; 
-          startGame()
-       }
+function initializeGame() {
 
-        
-    },1000)
+  startGame()
+  setInterval(countdown, 1000)
+}
+
+function countdown (){
+  numberCount++
+
+  document.getElementById("timer").innerHTML = "<b>" + remainingTime + "</b>"
+  console.log(remainingTime--)
+
+  if (numberCount === 15 && index < questions.length) {
+
+    index++
+    numberCount = 0;
+    startGame()
+  }
+
 }
 
